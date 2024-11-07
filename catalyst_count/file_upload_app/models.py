baace1272948bf352a635eb3a26db891cd82ee3e
mangeshahirwar <149577194+mangeshahirwar@users.noreply.github.com>
2024-11-07 17:06:02 +0530

    Delete catalyst_count directory

--- catalyst_count/file_upload_app/models.py
+++
@@ -1,9 +0,0 @@
-# Create your models here.
-
-from django.db import models
-
-class UploadedFile(models.Model):
-    file = models.FileField(upload_to='uploads/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-
